faa913b25ed59ddb16a6170bbbb78bb140860483
mrpmohiburrahman <mrpmohiburrahman@gmai.com>
2020-10-01 20:45:27 +0600

    💬 add descriptive comment to controllers/auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,10 @@ const User = require("../models/User");
 
 exports.register = async (req, res, next) => {
   try {
+    //1. we take the data from client
     const { name, email, password, bio } = req.body;
+
+    //2. validating if the user already exists.
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
@@ -11,6 +14,7 @@ exports.register = async (req, res, next) => {
         .json({ success: false, msg: "User already exists" });
     }
 
+    //creating the user object and save to our mongo db
     const user = await User.create({ name, email, password, bio });
 
     res.status(200).json({ success: true, user });
